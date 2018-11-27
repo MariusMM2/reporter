25d4f58eb9ca97efbbb4772b8f2e46f80892b8f5
MariusMM2 <dmunteanu91@yahoo.com>
2018-11-27 16:15:46 +0100

    animations improvements

--- app/src/main/java/com/marius/reporter/utils/anim/ViewElevator.java
+++ app/src/main/java/com/marius/reporter/utils/anim/ViewElevator.java
@@ -1,24 +1,22 @@
 package com.marius.reporter.utils.anim;
 
-import android.animation.ValueAnimator;
+import android.animation.ObjectAnimator;
 import android.support.annotation.DimenRes;
 import android.support.annotation.NonNull;
 import android.view.View;
-import android.view.animation.OvershootInterpolator;
+import android.view.animation.LinearInterpolator;
 
 public class ViewElevator {
-    public static int duration = 1000;
+    public static int duration = 250;
 
-    public static void elevate(@NonNull View view, @DimenRes int elevation) {
-        elevate(view, elevation, duration);
+    public static ObjectAnimator elevate(@NonNull View view, @DimenRes int elevation) {
+        return elevate(view, elevation, duration);
     }
 
-    public static void elevate(@NonNull View view, @DimenRes int elevation, int duration) {
-        ValueAnimator valueAnimator = new ValueAnimator();
-        valueAnimator.setInterpolator(new OvershootInterpolator());
-        valueAnimator.setFloatValues(view.getElevation(), view.getResources().getDimension(elevation));
-        valueAnimator.setDuration(duration);
-        valueAnimator.addUpdateListener(animation -> view.setElevation((float) animation.getAnimatedValue()));
-        valueAnimator.start();
+    public static ObjectAnimator elevate(@NonNull View view, @DimenRes int elevation, int duration) {
+        ObjectAnimator animator = ObjectAnimator.ofFloat(view, "elevation", view.getResources().getDimension(elevation));
+        animator.setDuration(duration);
+        animator.setInterpolator(new LinearInterpolator());
+        return animator;
     }
 }
--- app/src/main/java/com/marius/reporter/utils/anim/ViewTranslator.java
+++ app/src/main/java/com/marius/reporter/utils/anim/ViewTranslator.java
@@ -1,12 +1,18 @@
 package com.marius.reporter.utils.anim;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.AnimatorSet;
+import android.animation.ObjectAnimator;
+import android.annotation.SuppressLint;
 import android.view.View;
-import android.view.ViewPropertyAnimator;
 import android.view.animation.AnticipateInterpolator;
+import android.view.animation.BounceInterpolator;
 import android.view.animation.OvershootInterpolator;
 import com.marius.reporter.R;
 
 public class ViewTranslator {
+
     public enum Direction {
         LEFT,
         RIGHT,
@@ -15,67 +21,99 @@ public class ViewTranslator {
     }
 
     public static int duration = 500;
-    public static void moveToBehind(View mainView, View coverView) {
+
+    public static void moveToBehind(View mainView, View converView) {
+        moveToBehind(mainView, converView, duration);
+    }
+
+    public static void moveToBehind(View mainView, View coverView, int duration) {
         final int[] mainViewPos = new int[2]; mainView.getLocationOnScreen(mainViewPos);
         final int[] coverViewPos = new int[2]; coverView.getLocationOnScreen(coverViewPos);
         final float mainViewCenterX = mainViewPos[1] + (float)mainView.getWidth()  / 2;
         final float coverViewCenterX = coverViewPos[1] + (float)coverView.getWidth()  / 2;
-        if (mainViewCenterX != coverViewCenterX) {
-            mainView.animate().translationY((coverViewCenterX - mainViewCenterX))
-                    .setDuration(duration)
-                    .setInterpolator(new AnticipateInterpolator())
-                    .start();
-            ViewElevator.elevate(coverView, R.dimen.item_time_selected);
-        }
+
+        if (mainViewCenterX == coverViewCenterX) return;
+
+        ObjectAnimator hide = ObjectAnimator.ofFloat(mainView, "translationY", coverViewCenterX - mainViewCenterX);
+        hide.setDuration((int)(duration*0.66));
+        hide.setInterpolator(new AnticipateInterpolator());
+
+        ObjectAnimator elevate = ViewElevator.elevate(coverView, R.dimen.view_elevated, duration);
+        ObjectAnimator rotate = ObjectAnimator.ofFloat(coverView, "rotation", 90);
+        rotate.setDuration(duration);
+        rotate.setInterpolator(new BounceInterpolator());
+
+        AnimatorSet animations = new AnimatorSet();
+        animations.play(hide).after(elevate);
+        animations.play(elevate).with(rotate);
+        animations.start();
     }
 
     public static void moveFromBehind(View mainView, View coverView) {
-        mainView.animate().translationY(0)
-                .setDuration(duration)
-                .setInterpolator(new OvershootInterpolator())
-                .start();
-        ViewElevator.elevate(coverView, R.dimen.item_time_normal);
+        moveFromBehind(mainView, coverView, duration/2);
     }
 
-    public static void moveOffscreen(View view, Direction direction) {
-        moveOffscreen(view, direction, null);
+    public static void moveFromBehind(View mainView, View coverView, int duration) {
+        ObjectAnimator show = ObjectAnimator.ofFloat(mainView, "translationY", 0);
+        show.setDuration((int)(duration*0.66));
+        show.setInterpolator(new OvershootInterpolator());
+
+        ObjectAnimator elevate = ViewElevator.elevate(coverView, R.dimen.view_normal, duration);
+        ObjectAnimator rotate = ObjectAnimator.ofFloat(coverView, "rotation", 0);
+        rotate.setDuration(duration);
+        rotate.setInterpolator(new BounceInterpolator());
+
+        AnimatorSet animations = new AnimatorSet();
+        animations.play(show).before(elevate);
+        animations.play(elevate).with(rotate);
+        animations.start();
     }
 
     public static void moveOffscreen(View view, Direction direction, Runnable endAction) {
-        ViewPropertyAnimator animator = view.animate().setDuration(duration).setInterpolator(new AnticipateInterpolator())
-                .withEndAction(() -> {
-                    moveOffscreenReset(view, direction);
-                    if (endAction != null)
-                        endAction.run();
-                });
-        view.getHeight();
+        float value = 1000;
         switch (direction) {
-            case LEFT:
-                break;
-            case RIGHT:
-                animator.translationX(1000).start();
-                break;
-            case UP:
-                break;
-            case DOWN:
+            case LEFT: case UP:
+                value *= -1;
                 break;
         }
+        @SuppressLint("ObjectAnimatorBinding")
+        ObjectAnimator hide  = ObjectAnimator.ofFloat(view, getDirection(direction), value);
+        hide.setDuration(duration);
+        hide.setInterpolator(new AnticipateInterpolator());
+
+        AnimatorSet animations = new AnimatorSet();
+        animations.play(moveOffscreenReset(view, direction))
+                .after(hide);
+        animations.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                if (endAction != null) {
+                    endAction.run();
+                }
+            }
+        });
+        animations.start();
     }
 
-    public static void moveOffscreenReset(View view, Direction direction) {
-        moveOffscreenReset(view, direction, null);
+    private static ObjectAnimator moveOffscreenReset(View view, Direction direction) {
+
+        ObjectAnimator reveal = ObjectAnimator.ofFloat(view, getDirection(direction), 0);
+        reveal.setDuration(duration);
+        reveal.setInterpolator(new OvershootInterpolator());
+        return reveal;
     }
 
-    public static void moveOffscreenReset(View view, Direction direction, Runnable endAction) {
-        ViewPropertyAnimator animator = view.animate().setDuration(duration).setInterpolator(new OvershootInterpolator())
-                .withEndAction(endAction);
-        switch (direction) {
+    private static String getDirection(Direction direction) {
+        String translation = "translation";
+        switch(direction) {
             case LEFT: case RIGHT:
-                animator.translationX(0).start();
+                translation += "X";
                 break;
             case UP: case DOWN:
-                animator.translationY(0).start();
+                translation += "Y";
                 break;
         }
+        return translation;
     }
 }
