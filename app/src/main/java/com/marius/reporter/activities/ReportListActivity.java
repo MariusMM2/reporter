cdb2258bef28d0f0de49319b6a87d4cf372f7fd6
marius <cepulameavrei>
2019-03-09 20:08:03 +0100

    fast report click no longer open multiple pager activities

--- app/src/main/java/com/marius/reporter/activities/ReportListActivity.java
+++ app/src/main/java/com/marius/reporter/activities/ReportListActivity.java
@@ -7,9 +7,12 @@ import com.marius.reporter.Report;
 import com.marius.reporter.fragments.ReportFragment;
 import com.marius.reporter.fragments.ReportListFragment;
 
+import java.util.UUID;
+
 public class ReportListActivity extends SingleFragmentActivity implements ReportListFragment.Callbacks, ReportFragment.Callbacks {
     @SuppressWarnings("unused")
     private static final String TAG = ReportListActivity.class.getSimpleName();
+    private static UUID mQueuedReportId;
 
     @Override
     protected Fragment createFragment() {
@@ -18,8 +21,11 @@ public class ReportListActivity extends SingleFragmentActivity implements Report
 
     @Override
     public void onReportSelected(Report report) {
-        Intent intent = ReportPagerActivity.newIntent(this, report.getId());
-        startActivity(intent);
+        if (mQueuedReportId == null) {
+            mQueuedReportId = report.getId();
+            Intent intent = ReportPagerActivity.newIntent(this, report.getId());
+            startActivity(intent);
+        }
     }
 
     @Override
@@ -27,5 +33,11 @@ public class ReportListActivity extends SingleFragmentActivity implements Report
         ReportListFragment listFragment = (ReportListFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.fragment_container);
         listFragment.updateUI();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mQueuedReportId = null;
     }
 }
